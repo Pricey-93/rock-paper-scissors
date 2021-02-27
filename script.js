79db0f6f32f63c2591105f1599b33cfe03c943de
Dan <danieljamieprice@gmail.com>
2021-02-27 23:36:06 +0000

    make scores global. add eventlisteners to weapon-selections. increment scores.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+"use strict"
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     let result = Math.floor(Math.random() * Math.floor(3));
     if (result === 0) return "rock";
@@ -11,22 +15,33 @@ function playRound(playerSelection, computerSelection) {
         return "Draw !";
     else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissors" && computerSelection === "paper")) 
+        (playerSelection === "scissors" && computerSelection === "paper")) {
+        playerScore++;
+        document.getElementById("player-score").textContent = playerScore;
         return winningMessage;
+    }
     else 
+        computerScore++;
+        document.getElementById("computer-score").textContent = computerScore;
         return losingMessage;
 }
 function game() {
-    let temp;
-    let playerSelection;
     let computerSelection;
-    temp = prompt("Choose your weapon: rock, paper or scissors?", "rock");
-    playerSelection = temp.toLowerCase();
-    computerSelection = computerPlay();
-    return playRound(playerSelection, computerSelection);  
+    let playerSelection;
+    const items = document.querySelectorAll(".weapon-selection");
+    items.forEach(item => {
+        item.addEventListener("click", (e) => {
+            console.log(e.target);
+            var temp = item.textContent;
+            playerSelection = temp.toLocaleLowerCase();
+            console.log(playerSelection);
+            computerSelection = computerPlay();
+            console.log(computerSelection);
+            document.getElementById("result-text").textContent = playRound(playerSelection, computerSelection);
+        })
+    })
 }
-    console.log(game());
-    console.log(game());
-    console.log(game());
-    console.log(game());
-    console.log(game());
+game();
+
+
+
